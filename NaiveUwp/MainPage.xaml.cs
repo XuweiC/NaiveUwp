41d91a75bd45a9d0597c7f66d43c0d2099bf7db8
XuweiC <845709030@qq.com>
2018-04-01 19:57:10 +0800

    改用MediaPlayerElement

--- NaiveUwp/MainPage.xaml.cs
+++ NaiveUwp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,46 @@ namespace NaiveUwp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        MediaElement mediaElement = new MediaElement();
+        MediaPlayerElement mediaPlayerElement = new MediaPlayerElement();
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Volumn_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayButton.Content.ToString() == "Play")
+            {
+                PlayButton.Content = "Pause";
+                mediaElement.Play();
+            }
+            else
+            {
+                PlayButton.Content = "Play";
+                mediaElement.Pause();
+            }
+        }
+
+        private async void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
+            picker.FileTypeFilter.Add(".mp4");
+            picker.FileTypeFilter.Add(".mp3");
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(FileAccessMode.Read);
+                // 指定需要让 MediaElement 播放的媒体流
+                mediaElement.SetSource(stream, file.ContentType);
+            }
+            mediaElement.Play();
         }
     }
 }
